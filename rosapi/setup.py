27c2c6c5481fa7c26d12622d9d79e15e4e8e8676
Jihoon Lee <jihoonlee.in@gmail.com>
2014-03-28 09:20:22 +0900

    Rebase from 'upstream'

--- rosapi/setup.py
+++ rosapi/setup.py
@@ -5,8 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['rosapi'],
-    package_dir={'': 'src'},
-    scripts = ['scripts/rosapi']
+    package_dir={'': 'src'}
 )
 
 setup(**d)
