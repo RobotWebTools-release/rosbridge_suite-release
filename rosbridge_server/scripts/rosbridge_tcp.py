7049abd4149d8d0dbcef5bacc81175818c0b7427
Jihoon Lee <lee@magazino.eu>
2017-01-25 10:28:37 +0100

    Rebase from 'upstream'

--- rosbridge_server/scripts/rosbridge_tcp.py
+++ rosbridge_server/scripts/rosbridge_tcp.py
@@ -3,6 +3,13 @@
 from rospy import init_node, get_param, loginfo, logerr, on_shutdown
 from rosbridge_server import RosbridgeTcpSocket
 
+from rosbridge_library.capabilities.advertise import Advertise
+from rosbridge_library.capabilities.publish import Publish
+from rosbridge_library.capabilities.subscribe import Subscribe
+from rosbridge_library.capabilities.advertise_service import AdvertiseService
+from rosbridge_library.capabilities.unadvertise_service import UnadvertiseService
+from rosbridge_library.capabilities.call_service import CallService
+
 from functools import partial
 from signal import signal, SIGINT, SIG_DFL
 
@@ -52,7 +59,20 @@ if __name__ == "__main__":
             if max_message_size == "None":
                 max_message_size = None
 
-
+            # Get the glob strings and parse them as arrays.
+            RosbridgeTcpSocket.topics_glob = [
+                    element.strip().strip("'")
+                    for element in get_param('~topics_glob', '')[1:-1].split(',')
+                    if len(element.strip().strip("'")) > 0]
+            RosbridgeTcpSocket.services_glob = [
+                    element.strip().strip("'")
+                    for element in get_param('~services_glob', '')[1:-1].split(',')
+                    if len(element.strip().strip("'")) > 0]
+            RosbridgeTcpSocket.params_glob = [
+                    element.strip().strip("'")
+                    for element in get_param('~params_glob', '')[1:-1].split(',')
+                    if len(element.strip().strip("'")) > 0]
+        
             # update parameters if provided via commandline
             # .. could implemented 'better' (value/type checking, etc.. )
             if "--port" in sys.argv:
@@ -129,6 +149,54 @@ if __name__ == "__main__":
             RosbridgeTcpSocket.fragment_timeout = fragment_timeout
             RosbridgeTcpSocket.delay_between_messages = delay_between_messages
             RosbridgeTcpSocket.max_message_size = max_message_size
+
+
+            if "--topics_glob" in sys.argv:
+                idx = sys.argv.index("--topics_glob") + 1
+                if idx < len(sys.argv):
+                    value = sys.argv[idx]
+                    if value == "None":
+                        RosbridgeTcpSocket.topics_glob = []
+                    else:
+                        RosbridgeTcpSocket.topics_glob = [element.strip().strip("'") for element in value[1:-1].split(',')]
+                else:
+                    print "--topics_glob argument provided without a value. (can be None or a list)"
+                    sys.exit(-1)
+
+            if "--services_glob" in sys.argv:
+                idx = sys.argv.index("--services_glob") + 1
+                if idx < len(sys.argv):
+                    value = sys.argv[idx]
+                    if value == "None":
+                        RosbridgeTcpSocket.services_glob = []
+                    else:
+                        RosbridgeTcpSocket.services_glob = [element.strip().strip("'") for element in value[1:-1].split(',')]
+                else:
+                    print "--services_glob argument provided without a value. (can be None or a list)"
+                    sys.exit(-1)
+
+            if "--params_glob" in sys.argv:
+                idx = sys.argv.index("--params_glob") + 1
+                if idx < len(sys.argv):
+                    value = sys.argv[idx]
+                    if value == "None":
+                        RosbridgeTcpSocket.params_glob = []
+                    else:
+                        RosbridgeTcpSocket.params_glob = [element.strip().strip("'") for element in value[1:-1].split(',')]
+                else:
+                    print "--params_glob argument provided without a value. (can be None or a list)"
+                    sys.exit(-1)
+
+            # To be able to access the list of topics and services, you must be able to access the rosapi services.
+            if RosbridgeTcpSocket.services_glob:
+                RosbridgeTcpSocket.services_glob.append("/rosapi/*")
+
+            Subscribe.topics_glob = RosbridgeTcpSocket.topics_glob
+            Advertise.topics_glob = RosbridgeTcpSocket.topics_glob
+            Publish.topics_glob = RosbridgeTcpSocket.topics_glob
+            AdvertiseService.services_glob = RosbridgeTcpSocket.services_glob
+            UnadvertiseService.services_glob = RosbridgeTcpSocket.services_glob
+            CallService.services_glob = RosbridgeTcpSocket.services_glob
 
             """
             ...END (parameter handling)
