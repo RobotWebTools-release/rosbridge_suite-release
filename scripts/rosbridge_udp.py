5557e7f51f223a859cf67be7e19e2e7e423b6afb
Jihoon Lee <lee@magazino.eu>
2017-01-25 10:29:09 +0100

    Rebase from 'release/kinetic/rosbridge_server'

--- scripts/rosbridge_udp.py
+++ scripts/rosbridge_udp.py
@@ -37,8 +37,17 @@ import sys
 from socket import error
 from twisted.internet import reactor
 from rosbridge_server import RosbridgeUdpSocket,RosbridgeUdpFactory
+
+from rosbridge_library.capabilities.advertise import Advertise
+from rosbridge_library.capabilities.publish import Publish
+from rosbridge_library.capabilities.subscribe import Subscribe
+from rosbridge_library.capabilities.advertise_service import AdvertiseService
+from rosbridge_library.capabilities.unadvertise_service import UnadvertiseService
+from rosbridge_library.capabilities.call_service import CallService
+
 def shutdown_hook():
     reactor.stop()
+
 if __name__ == "__main__":
     rospy.init_node("rosbridge_websocket")
     rospy.on_shutdown(shutdown_hook)    # register shutdown hook to stop the server
@@ -55,6 +64,20 @@ if __name__ == "__main__":
                                                           RosbridgeUdpSocket.max_message_size)
     if RosbridgeUdpSocket.max_message_size == "None":
         RosbridgeUdpSocket.max_message_size = None
+
+    # Get the glob strings and parse them as arrays.
+    RosbridgeUdpSocket.topics_glob = [
+        element.strip().strip("'")
+        for element in rospy.get_param('~topics_glob', '')[1:-1].split(',')
+        if len(element.strip().strip("'")) > 0]
+    RosbridgeUdpSocket.services_glob = [
+        element.strip().strip("'")
+        for element in rospy.get_param('~services_glob', '')[1:-1].split(',')
+        if len(element.strip().strip("'")) > 0]
+    RosbridgeUdpSocket.params_glob = [
+        element.strip().strip("'")
+        for element in rospy.get_param('~params_glob', '')[1:-1].split(',')
+        if len(element.strip().strip("'")) > 0]
 
     # if authentication should be used
     RosbridgeUdpSocket.authenticate = rospy.get_param('~authenticate', False)
@@ -104,6 +127,53 @@ if __name__ == "__main__":
         else:
             print "--max_message_size argument provided without a value. (can be None or <Integer>)"
             sys.exit(-1)
+
+    if "--topics_glob" in sys.argv:
+        idx = sys.argv.index("--topics_glob") + 1
+        if idx < len(sys.argv):
+            value = sys.argv[idx]
+            if value == "None":
+                RosbridgeUdpSocket.topics_glob = []
+            else:
+                RosbridgeUdpSocket.topics_glob = [element.strip().strip("'") for element in value[1:-1].split(',')]
+        else:
+            print "--topics_glob argument provided without a value. (can be None or a list)"
+            sys.exit(-1)
+
+    if "--services_glob" in sys.argv:
+        idx = sys.argv.index("--services_glob") + 1
+        if idx < len(sys.argv):
+            value = sys.argv[idx]
+            if value == "None":
+                RosbridgeUdpSocket.services_glob = []
+            else:
+                RosbridgeUdpSocket.services_glob = [element.strip().strip("'") for element in value[1:-1].split(',')]
+        else:
+            print "--services_glob argument provided without a value. (can be None or a list)"
+            sys.exit(-1)
+
+    if "--params_glob" in sys.argv:
+        idx = sys.argv.index("--params_glob") + 1
+        if idx < len(sys.argv):
+            value = sys.argv[idx]
+            if value == "None":
+                RosbridgeUdpSocket.params_glob = []
+            else:
+                RosbridgeUdpSocket.params_glob = [element.strip().strip("'") for element in value[1:-1].split(',')]
+        else:
+            print "--params_glob argument provided without a value. (can be None or a list)"
+            sys.exit(-1)
+
+    # To be able to access the list of topics and services, you must be able to access the rosapi services.
+    if RosbridgeUdpSocket.services_glob:
+        RosbridgeUdpSocket.services_glob.append("/rosapi/*")
+
+    Subscribe.topics_glob = RosbridgeUdpSocket.topics_glob
+    Advertise.topics_glob = RosbridgeUdpSocket.topics_glob
+    Publish.topics_glob = RosbridgeUdpSocket.topics_glob
+    AdvertiseService.services_glob = RosbridgeUdpSocket.services_glob
+    UnadvertiseService.services_glob = RosbridgeUdpSocket.services_glob
+    CallService.services_glob = RosbridgeUdpSocket.services_glob
 
     ##################################################
     # Done with parameter handling                   #
