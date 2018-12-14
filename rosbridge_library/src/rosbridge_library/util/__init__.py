663f9e90ea9312607d88cd5814ec0a0ba73ecdfe
Jihoon Lee <jihoonlee.in@gmail.com>
2018-12-14 18:13:08 +0900

    Rebase from 'upstream'

--- rosbridge_library/src/rosbridge_library/util/__init__.py
+++ rosbridge_library/src/rosbridge_library/util/__init__.py
@@ -6,3 +6,22 @@ except ImportError:
         import simplejson as json
     except ImportError:
         import json
+
+# Differing string types for Python 2 and 3
+import sys
+if sys.version_info >= (3, 0):
+    string_types = (str,)
+    from io import StringIO
+else:
+    string_types = (str, unicode)
+    from StringIO import StringIO
+
+import bson
+try:
+    bson.BSON
+except AttributeError:
+    raise Exception(
+        "BSON installation does not support all necessary features. "
+        "Please use the MongoDB BSON implementation. "
+        "See: https://github.com/RobotWebTools/rosbridge_suite/issues/198"
+    )
