2b6ffde1e1be8e3d5f79a494a410f19565d63820
Jacob Bandes-Storch <jacob@foxglove.dev>
2022-06-13 10:40:44 -0700

    Rebase from 'upstream'

--- rosbridge_library/test/internal/publishers/test_multi_unregistering.py
+++ rosbridge_library/test/internal/publishers/test_multi_unregistering.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python
-import sys
+
+import os
 import rospy
-import rostest
+import rosunit
 import unittest
 
-from time import sleep, time
+from time import sleep
 
-from rosbridge_library.internal.publishers import *
-from rosbridge_library.internal.topics import *
+from rosbridge_library.internal.publishers import MultiPublisher
 from rosbridge_library.internal import ros_loader
-from rosbridge_library.internal.message_conversion import *
-from std_msgs.msg import String, Int32
+
+
+PKG = 'rosbridge_library'
+NAME = 'test_multi_unregistering'
 
 
 class TestMultiUnregistering(unittest.TestCase):
 
     def setUp(self):
-        rospy.init_node("test_multi_unregistering")
+        rospy.init_node(NAME, log_level=rospy.DEBUG)
 
     def test_publish_once(self):
         """ Make sure that publishing works """
@@ -25,6 +27,7 @@ class TestMultiUnregistering(unittest.TestCase):
         msg = {"data": "why halo thar"}
 
         received = {"msg": None}
+
         def cb(msg):
             received["msg"] = msg
 
@@ -32,7 +35,7 @@ class TestMultiUnregistering(unittest.TestCase):
         p = MultiPublisher(topic, msg_type)
         p.publish(msg)
 
-        sleep(1)
+        sleep(1)  # Time to publish and receive
 
         self.assertEqual(received["msg"].data, msg["data"])
 
@@ -43,6 +46,7 @@ class TestMultiUnregistering(unittest.TestCase):
         msg = {"data": "why halo thar"}
 
         received = {"msg": None}
+
         def cb(msg):
             received["msg"] = msg
 
@@ -50,40 +54,22 @@ class TestMultiUnregistering(unittest.TestCase):
         p = MultiPublisher(topic, msg_type)
         p.publish(msg)
 
-        sleep(1)
+        sleep(1)  # Time to publish and receive
 
         self.assertEqual(received["msg"].data, msg["data"])
 
         p.unregister()
-        # The publisher went away at time T. Here's the timeline of the events:
-        # T+1 seconds - the subscriber will retry to reconnect - fail
-        # T+3 seconds - the subscriber will retry to reconnect - fail
-        # T+5 seconds - publish msg -> it's gone
-        # T+7 seconds - the subscriber will retry to reconnect - success
-        # T+8 seconds - publish msg -> OK
-        # T+11 seconds - we receive the message. Looks like a bug in reconnection...
-        #                https://github.com/ros/ros_comm/blob/indigo-devel/clients/rospy/src/rospy/impl/tcpros_base.py#L733
-        #                That line should probably be indented.
-        sleep(5)
+        sleep(5)  # Time to unregister
 
-        received["msg"] = None
+        received["msg"] = None  # Reset received
         self.assertIsNone(received["msg"])
         p = MultiPublisher(topic, msg_type)
         p.publish(msg)
 
-        self.assertIsNone(received["msg"])
+        sleep(1)   # Time to publish and receive
 
-        sleep(3)
-        p.publish(msg)
-        sleep(2)
-        # Next two lines should be removed when this is fixed:
-        # https://github.com/ros/ros_comm/blob/indigo-devel/clients/rospy/src/rospy/impl/tcpros_base.py#L733
-        self.assertIsNone(received["msg"])
-        sleep(3)
         self.assertEqual(received["msg"].data, msg["data"])
 
 
-PKG = 'rosbridge_library'
-NAME = 'test_multi_unregistering'
 if __name__ == '__main__':
-    rostest.unitrun(PKG, NAME, TestMultiUnregistering)
+    rosunit.unitrun(PKG, NAME, TestMultiUnregistering)
